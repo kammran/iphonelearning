5556b3bd9e164f37b6f1ff807ccf564d43c135c8
Java2Enterprise <Java2Enterprise@28abb18a-2c56-11df-af82-b5ce7a3b21f7>
2010-03-22 18:03:58 +0000

    Added project.

---
+++ View Switcher/Classes/YellowViewController.h
@@ -0,0 +1,16 @@
+//
+//  YellowViewController.h
+//  View Switcher
+//
+//  Created by James Wang on 3/23/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface YellowViewController : UIViewController {
+
+}
+
+@end
