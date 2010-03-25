e8d784bef71ba706838dfd4a435d4505b2bb69ef
Java2Enterprise <Java2Enterprise@28abb18a-2c56-11df-af82-b5ce7a3b21f7>
2010-03-25 14:58:14 +0000

    ggg

--- trunk/View Switcher/Classes/BlueViewController.m
+++ trunk/View Switcher/Classes/BlueViewController.m
@@ -11,6 +11,19 @@
 
 @implementation BlueViewController
 
+- (IBAction)blueButtonPressed:(id)sender {
+	UIAlertView *alert = [[UIAlertView alloc]
+						  initWithTitle:@"Blue View Button Pressed" 
+						  message:@"You pressed the button on the blue view" 
+						  delegate:nil
+						  cancelButtonTitle:@"Yep, I did." 
+						  otherButtonTitles:nil];
+	[alert show];
+	[alert release];
+	
+}
+
+
 /*
  // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
