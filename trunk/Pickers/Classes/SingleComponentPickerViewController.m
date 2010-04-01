d01d410fb1666fb3ad85e519051890d8d677a8cb
Java2Enterprise <Java2Enterprise@28abb18a-2c56-11df-af82-b5ce7a3b21f7>
2010-04-01 18:18:43 +0000

    Implemented SingleComponentPicker

--- trunk/Pickers/Classes/SingleComponentPickerViewController.m
+++ trunk/Pickers/Classes/SingleComponentPickerViewController.m
@@ -10,31 +10,44 @@
 
 
 @implementation SingleComponentPickerViewController
+@synthesize singlePicker;
+@synthesize pickerData;
 
-/*
- // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
-        // Custom initialization
-    }
-    return self;
+- (IBAction)buttonPressed {
+	NSInteger row = [singlePicker selectedRowInComponent:0];
+	NSString *selected = [pickerData objectAtIndex:row];
+	NSString *title = [[NSString alloc] initWithFormat:@"You selected %@", selected];
+	
+	UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title
+													message:@"Thank you for choosing" 
+												   delegate:nil 
+										  cancelButtonTitle:@"You are welcome" 
+										  otherButtonTitles:nil];
+	
+	[alert show];
+	[alert release];
+	[title release];
+	[selected release];
+	
 }
-*/
 
-/*
+
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
-    [super viewDidLoad];
+	NSLog(@"View Did Load");
+	NSArray *array = [[NSArray alloc] initWithObjects: @"iPhone", @"Android", @"Windows Mobile", @"Symbian", @"J2ME", nil];
+	self.pickerData = array;
+	[array release];
 }
-*/
+
 
 /*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-*/
+ // Override to allow orientations other than the default portrait orientation.
+ - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+ // Return YES for supported orientations
+ return (interfaceOrientation == UIInterfaceOrientationPortrait);
+ }
+ */
 
 - (void)didReceiveMemoryWarning {
 	// Releases the view if it doesn't have a superview.
@@ -46,12 +59,31 @@
 - (void)viewDidUnload {
 	// Release any retained subviews of the main view.
 	// e.g. self.myOutlet = nil;
+	self.pickerData = nil;
+	self.singlePicker = nil;
 }
 
 
 - (void)dealloc {
+	[self.pickerData release];
+	[self.singlePicker release];
     [super dealloc];
 }
 
+#pragma mark -
+#pragma mark Picker DataSource Methods
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+	return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+	return [self.pickerData count];
+}
+
+#pragma mark Picker Delegate Methods
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+	return [self.pickerData objectAtIndex:row];
+}
 
 @end
