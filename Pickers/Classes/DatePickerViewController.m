e0a0d4d73ef8b751c43d9ad423634eb6a132139c
Java2Enterprise <Java2Enterprise@28abb18a-2c56-11df-af82-b5ce7a3b21f7>
2010-04-01 09:14:59 +0000

    Modified

--- Pickers/Classes/DatePickerViewController.m
+++ Pickers/Classes/DatePickerViewController.m
@@ -10,31 +10,39 @@
 
 
 @implementation DatePickerViewController
+@synthesize datePicker;
 
-/*
- // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
-        // Custom initialization
-    }
-    return self;
+-(IBAction)buttonPressed {
+	NSDate *selected = [datePicker date];
+	NSString *message = [[NSString alloc] initWithFormat:
+						 @"The date and time you selected is : %@", selected];
+	UIAlertView *alert = [[UIAlertView alloc]
+						  initWithTitle:@"Date and Time Selected" 
+						  message:message
+						  delegate:nil
+						  cancelButtonTitle:@"Yes, I did." 
+						  otherButtonTitles:nil];
+	[alert show];
+	[alert release];
+	[message release];
 }
-*/
 
-/*
+
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
-    [super viewDidLoad];
+    NSDate *now = [[NSDate alloc] init];
+	[datePicker setDate:now animated:YES];
+	//	[super viewDidLoad];
 }
-*/
+
 
 /*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-*/
+ // Override to allow orientations other than the default portrait orientation.
+ - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+ // Return YES for supported orientations
+ return (interfaceOrientation == UIInterfaceOrientationPortrait);
+ }
+ */
 
 - (void)didReceiveMemoryWarning {
 	// Releases the view if it doesn't have a superview.
@@ -46,10 +54,13 @@
 - (void)viewDidUnload {
 	// Release any retained subviews of the main view.
 	// e.g. self.myOutlet = nil;
+	self.datePicker = nil;
+	[super viewDidUnload];
 }
 
 
 - (void)dealloc {
+	[datePicker release];
     [super dealloc];
 }
 
