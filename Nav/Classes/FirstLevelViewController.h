0b167ea6274f8391859cfb177d2a21bcde626df0
Java2Enterprise <Java2Enterprise@28abb18a-2c56-11df-af82-b5ce7a3b21f7>
2010-04-12 17:21:30 +0000

    Added Nav project.

---
+++ Nav/Classes/FirstLevelViewController.h
@@ -0,0 +1,16 @@
+//
+//  FirstLevelViewController.h
+//  Nav
+//
+//  Created by James Wang on 4/13/10.
+//  Copyright 2010 DerbySoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface FirstLevelViewController : UITableViewController {
+
+}
+
+@end
