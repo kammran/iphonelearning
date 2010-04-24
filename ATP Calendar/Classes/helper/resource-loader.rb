50103d803e8fa1ad1806ea135664903bb3cfccd8
Java2Enterprise <Java2Enterprise@28abb18a-2c56-11df-af82-b5ce7a3b21f7>
2010-04-24 15:14:49 +0000

    Working

--- ATP Calendar/Classes/helper/resource-loader.rb
+++ ATP Calendar/Classes/helper/resource-loader.rb
@@ -3,88 +3,100 @@
 require 'rubygems'
 require 'open-uri'
 require 'hpricot'
-
-
+require 'builder' 
 
 class ResourceLoader
 
 	def load
-		doc = open("Event-Calendar.aspx") { |f| Hpricot(f) }
-		elements = doc.search("//div[@class='calendarTable']")
+		xml = '' 
+		builder_doc = Builder::XmlMarkup.new(:target => xml, :indent => 2) 
+		builder_doc.instruct!
+
+		src_doc = open("Event-Calendar.aspx") { |f| Hpricot(f) }
+		elements = src_doc.search("//div[@class='calendarTable']")
+
 		elements.each do |monthElement|
 			month = monthElement.at('h4').inner_text	
-			puts month
-			monthElement.search("//tr[@class='calendarFilterItem']").each do |matchRow|
-				
-				#puts matchRow.inner_text
+			builder_doc.month(:name => month) do |builder_month|
+				monthElement.search("//tr[@class='calendarFilterItem']").each do |matchRow|
+					
+					builder_month.match do |builder_match|
 
-				next if !(matchRow.inner_text =~ /.*Singles.*Doubles.*/m)
-
-				columns = matchRow.search('td')				
-				columns.size.times do |index|
-					td = columns[index]
-					case index
-					when 0  
-						imgElement = td.at('img')
-						imgSrc = imgElement.nil? ? nil : imgElement.attributes['src']
-						puts imgSrc
-					when 1 
-						date = td.inner_text.strip
-						puts date
-					when 2
-						array = td.search('/strong')
-						name = array[0].inner_text.strip
-						puts name
-						cityAndCountry = array[1].inner_text.strip
-						array = cityAndCountry.split(',').map {|e| e.strip}
-						city = array[0]
-						country = array[1]
-						puts city
-						puts country
-					when 3
-						surface = td.inner_html.gsub('<br />', ' ').strip
-						puts surface
-					when 4
-						money = td.inner_text.strip
-						money =~ /(.*)\((.*)\)/m
-						prize = stripSafely($1) 
-						total = stripSafely($2)
-						puts prize
-						puts total
-					when 5
-						draw = stripSafely(td.inner_text)
-						draw =~ /SGL.*?(\d+).*DBL.*?(\d+)/m
-						sgl = stripSafely($1) 
-						dbl = stripSafely($2)
-						puts sgl
-						puts dbl
-					when 6 
-						ticketInfo = td.inner_html.strip
-						emailHref = td.at('a')
-						email = '' if emailHref.nil?
-						email = emailHref.attributes['href'].gsub(/mailto:(.*)/) {|m| $1} if !emailHref.nil?
-						puts email
-						tel = ticketInfo.gsub(/<a.*<\/a>/, '').gsub(/<br \/>/, '')
-						puts tel
-					when 7
-						winners = td.inner_text.strip
-						winners =~ /.*Singles.*:(.+)Doubles.*:(.+)/m
-						singleWinner = stripSafely($1) 
-						doubleWinners = stripSafely($2)
-						puts singleWinner
-						puts doubleWinners
+					columns = matchRow.search('td')				
+					columns.size.times do |index|
+						td = columns[index]
+						case index
+						when 0  
+							img_element = td.at('img')
+							img_src = img_element.nil? ? nil : img_element.attributes['src']
+							builder_match.category(category_of(img_src))
+						when 1 
+							date = td.inner_text.strip
+							builder_match.date(date)
+						when 2
+							array = td.search('/strong')
+							name = array[0].inner_text.strip
+							cityAndCountry = array[1].inner_text.strip
+							array = cityAndCountry.split(',').map {|e| e.strip}
+							city = array[0]
+							country = array[1]
+							builder_match.name(name)
+							builder_match.city(city)
+							builder_match.country(country)
+						when 3
+							surface = td.inner_html.gsub('<br />', ' ').strip
+							builder_match.surface(surface)
+						when 4
+							money = td.inner_text.strip
+							money =~ /(.*)\((.*)\)/m
+							prize = strip_safely($1) 
+							total = strip_safely($2)
+							builder_match.prize(prize)
+							builder_match.total(total)
+						when 5
+							draw = strip_safely(td.inner_text)
+							draw =~ /SGL.*?(\d+).*DBL.*?(\d+)/m
+							sgl = strip_safely($1) 
+							dbl = strip_safely($2)
+							builder_match.sgl(sgl)
+							builder_match.dbl(dbl)
+						when 6 
+							ticketInfo = td.inner_html.strip
+							emailHref = td.at('a')
+							email = '' if emailHref.nil?
+							email = emailHref.attributes['href'].gsub(/mailto:(.*)/) {|m| $1} if !emailHref.nil?
+							tel = ticketInfo.gsub(/<a.*<\/a>/, '').gsub(/<br \/>/, '')
+							builder_match.email(email)
+							builder_match.tel(tel)
+						when 7
+							winners = td.inner_text.strip
+							winners =~ /.*Singles.*:(.+)Doubles.*:(.+)/m
+							single_winner = strip_safely($1) 
+							double_winners = strip_safely($2)
+							builder_match.single_winner(single_winner)
+							builder_match.double_winners(double_winners)	
+						end
 					end
-				end
+					end					
 
-				puts '------------'
+				end
 			end
 		end
+		
+		xml
+
 	end
 
-	def stripSafely(obj)
+	def strip_safely(obj)
 		obj.nil? ? '' : obj.strip	
 	end
+	
+	def category_of(img_src)
+		#TODO implement
+		"ATP 250"
+	end
+
 end
 
 
-ResourceLoader.new.load
+puts ResourceLoader.new.load
