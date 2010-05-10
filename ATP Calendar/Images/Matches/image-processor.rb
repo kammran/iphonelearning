cc1496927d281c6b9857b06356ef41a82a22c038
Java2Enterprise <Java2Enterprise@28abb18a-2c56-11df-af82-b5ce7a3b21f7>
2010-05-10 17:18:18 +0000

    Modified way of load images

--- ATP Calendar/Images/Matches/image-processor.rb
+++ ATP Calendar/Images/Matches/image-processor.rb
@@ -7,6 +7,12 @@ class Rename
 	
 	include MiniMagick
 
+	@template_plist
+
+	def initialize
+		@template_plist = IO.readlines('template.plist').join
+	end
+
 	def do
 		Dir.foreach('.') do |dir|
 			unless dir =~ /\./ 
@@ -17,6 +23,7 @@ class Rename
 				Dir.foreach('.') do |file|
 					resize(file)
 				end
+				generate_plist(dir)
 				Dir.chdir('..')
 			end
 		end
@@ -24,7 +31,7 @@ class Rename
 	
 	def supported?(file)
 		#yield if file =~ /jpg|png/ 
-		yield if file =~ /png/ 
+		yield if file =~ /png/
 	end
 
 	def rename(dir, file)
@@ -60,6 +67,14 @@ class Rename
 			puts "Resizing #{file} from #{old_size} to #{new_size}"
 			image.resize new_size 
 		end
+	end
+	
+	def generate_plist(dir)
+		images = Dir.entries('.').select {|file| file =~ /png|jpg/}
+		image_count = images.size
+		image_type = images.empty?? '' : images.first.sub(/.*\.(.+)/) { $1 }
+		plist = @template_plist.gsub('${ImageCount}', image_count.to_s).gsub('${ImageType}', image_type)
+		File.open("#{dir}.plist", 'w') {|f| f.write(plist)}
 	end
 
 end
