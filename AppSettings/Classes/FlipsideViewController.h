f6d4c87ff06c9f64df9f189ee587c9cefef2d632
Java2Enterprise <Java2Enterprise@28abb18a-2c56-11df-af82-b5ce7a3b21f7>
2010-05-18 18:11:36 +0000

    Added AppSettings project

---
+++ AppSettings/Classes/FlipsideViewController.h
@@ -0,0 +1,27 @@
+//
+//  FlipsideViewController.h
+//  AppSettings
+//
+//  Created by James Wang on 5/19/10.
+//  Copyright DerbySoft 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol FlipsideViewControllerDelegate;
+
+
+@interface FlipsideViewController : UIViewController {
+	id <FlipsideViewControllerDelegate> delegate;
+}
+
+@property (nonatomic, assign) id <FlipsideViewControllerDelegate> delegate;
+- (IBAction)done;
+
+@end
+
+
+@protocol FlipsideViewControllerDelegate
+- (void)flipsideViewControllerDidFinish:(FlipsideViewController *)controller;
+@end
+
---
+++ AppSettings/Classes/MainViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainViewController.h
+//  AppSettings
+//
+//  Created by James Wang on 5/19/10.
+//  Copyright DerbySoft 2010. All rights reserved.
+//
+
+#import "FlipsideViewController.h"
+
+@interface MainViewController : UIViewController <FlipsideViewControllerDelegate> {
+}
+
+- (IBAction)showInfo;
+
+@end
