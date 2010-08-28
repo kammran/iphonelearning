78b35cc628c3c54c748822def209e4768026f8df
Java2Enterprise <Java2Enterprise@28abb18a-2c56-11df-af82-b5ce7a3b21f7>
2010-08-28 09:38:21 +0000

    Initial commit of Cosplaying

---
+++ trunk/Cosplaying/Classes/CosplayingAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  CosplayingAppDelegate.h
+//  Cosplaying
+//
+//  Created by James Wang on 8/28/10.
+//  Copyright DerbySoft 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CosplayingAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
