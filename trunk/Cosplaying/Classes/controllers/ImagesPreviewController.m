76ebb28652e976f76ad7daaee428cb5fc5792169
Java2Enterprise <Java2Enterprise@28abb18a-2c56-11df-af82-b5ce7a3b21f7>
2010-09-10 17:47:44 +0000

    Working

--- trunk/Cosplaying/Classes/controllers/ImagesPreviewController.m
+++ trunk/Cosplaying/Classes/controllers/ImagesPreviewController.m
@@ -19,51 +19,68 @@
 @synthesize imageView2;
 @synthesize imageView3;
 @synthesize imageView4;
+@synthesize datePicker;
+@synthesize jumpToDateButton;
 
+- (void)downloadImage:(NSArray *)array {
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	UIImageView *imageView = [array objectAtIndex:0];
+	NSString *url = [array objectAtIndex:1];
+	imageView.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:url]]];
+	[pool release];
+}
 
 - (void)render:(NSArray *) array {
+	if ([array count] ==0) {
+		[@"Sorry, no images for your selection." showInDialog];
+	}
+	
 	int i = 1;
 	for (NSDictionary *each in array) {
 		NSString *folder = [each valueForKey:@"folder"];
 		NSString *name = [each valueForKey:@"name"];
 		NSString *url = [NSString stringWithFormat:@"%@/%@/%@", IMAGE_SERVER, folder, name];
-		NSLog(@"%@", url);
-		
-		if (i <= 4) {
+		if (i <= IMAGES_PER_PAGE) {
 			UIImageView *imageView = [self valueForKey:[NSString stringWithFormat:@"imageView%d", i]];
-			imageView.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:url]]];
+			NSArray *array = [NSArray arrayWithObjects:imageView, url, nil];
+			[self performSelectorInBackground:@selector(downloadImage:) withObject:array];
 		}
 		
 		i++;
 	}
 }
 
+
 #pragma mark -
 #pragma mark Remoting Methods
-- (void) requestRecents {
+
+- (void) request {
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-	
-	NSString *q = @"recent";
-	int offset = 0;
-	NSString *url = [[NSString stringWithFormat:@"%@/images?q=%@&offset=%d", SERVICE_URL, q, offset] 
+
+	NSString *url = [[NSString stringWithFormat:@"%@/images?q=%@&offset=%d&keyword=%@", SERVICE_URL, q, offset, keyword] 
 					 stringByAddingPercentEscapesUsingEncoding:NSStringEncodingConversionExternalRepresentation];
 	
 	
 	NSString *response = [[NSString alloc] initWithContentsOfURL:[NSURL URLWithString:url]];
 	NSArray *array = [response JSONValue];
-	
 	[self render:array];
-	
 	[response release];
-
+	
 	[pool release];
 }
 
+- (void) requestInBackground {
+	[self performSelectorInBackground:@selector(request) withObject:nil];
+}
 
+- (void) requestRecents {
+	q = @"recent";
+	[self requestInBackground];
+}
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	[self performSelectorInBackground:@selector(requestRecents) withObject:nil];
+	[self requestRecents];
 }
 
 - (void)viewDidUnload {
@@ -120,7 +137,14 @@
 
 - (IBAction)segmentPressed:(id) sender {
 	UISegmentedControl *segmentedControl = (UISegmentedControl *) sender;
-	[[NSString stringWithFormat:@"%d", segmentedControl.selectedSegmentIndex] showInDialog];
+	if (segmentedControl.selectedSegmentIndex == 2) {
+		//jump to date
+		self.datePicker.hidden = NO;
+		self.jumpToDateButton.hidden = NO;
+	} else {
+		self.datePicker.hidden = YES;
+		self.jumpToDateButton.hidden = YES;		
+	}
 }
 
 #pragma mark -
@@ -135,6 +159,46 @@
 - (void)searchBarCancelButtonClicked:(UISearchBar *)theSearchBar {
 	[self.searchBar resignFirstResponder];
 	self.searchBar.hidden = YES;
+}
+
+#pragma mark -
+#pragma mark UI Buttons Methods
+
+- (void)resetImages2Downloading {
+	for (int i = 1; i <= IMAGES_PER_PAGE; i++) {
+		UIImageView *imageView = [self valueForKey:[NSString stringWithFormat:@"imageView%d", i]];
+		imageView.image = [UIImage imageNamed:@"downloading.png"];
+	}
+}
+
+- (IBAction)previousPressed {
+	offset -= IMAGES_PER_PAGE;
+	if (offset < 0) {
+		offset = 0;
+		return;
+	}
+	[self resetImages2Downloading];
+	[self requestInBackground];
+}
+
+- (IBAction)nextPressed {
+	offset += IMAGES_PER_PAGE;
+	[self resetImages2Downloading];
+	[self requestInBackground];
+}
+
+- (IBAction)jumpToDatePressed {
+	self.datePicker.hidden = YES;
+	self.jumpToDateButton.hidden = YES;
+	
+	NSDateFormatter* formatter = [[NSDateFormatter alloc] init];
+	[formatter setDateFormat:@"yyyy-MM-dd"];
+	keyword = [formatter stringFromDate:self.datePicker.date];
+	[formatter release];
+	
+	q = @"folder";
+	offset = 0;
+	[self requestInBackground];
 }
 
 @end
