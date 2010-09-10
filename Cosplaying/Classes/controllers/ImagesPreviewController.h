ddd8969e33643f39a0fd5d6af11e637d1844b2ab
Java2Enterprise <Java2Enterprise@28abb18a-2c56-11df-af82-b5ce7a3b21f7>
2010-09-10 14:45:24 +0000

    Working

--- Cosplaying/Classes/controllers/ImagesPreviewController.h
+++ Cosplaying/Classes/controllers/ImagesPreviewController.h
@@ -8,14 +8,25 @@
 
 #import <UIKit/UIKit.h>
 
+#define SERVICE_URL @"http://localhost:8080"
+#define IMAGE_SERVER @"http://freezedisk.googlecode.com/svn/trunk/iphone/cosplaying/images/real"
 
 @interface ImagesPreviewController : UIViewController <UISearchBarDelegate> {
 
 	UISearchBar *searchBar;
+	UIImageView *imageView1;
+	UIImageView *imageView2;
+	UIImageView *imageView3;
+	UIImageView *imageView4;
 	
 }
 
 @property (nonatomic, retain) IBOutlet UISearchBar *searchBar;
+@property (nonatomic, retain) IBOutlet UIImageView *imageView1;
+@property (nonatomic, retain) IBOutlet UIImageView *imageView2;
+@property (nonatomic, retain) IBOutlet UIImageView *imageView3;
+@property (nonatomic, retain) IBOutlet UIImageView *imageView4;
+
 
 - (IBAction)searchButtonPressed;
 - (IBAction)segmentPressed:(id) sender;
