53f3e6eb5f53906284d7f874969eaa0205edd843
Java2Enterprise <Java2Enterprise@28abb18a-2c56-11df-af82-b5ce7a3b21f7>
2010-09-10 17:47:44 +0000

    Working

--- Cosplaying/Classes/controllers/ImagesPreviewController.h
+++ Cosplaying/Classes/controllers/ImagesPreviewController.h
@@ -10,6 +10,7 @@
 
 #define SERVICE_URL @"http://localhost:8080"
 #define IMAGE_SERVER @"http://freezedisk.googlecode.com/svn/trunk/iphone/cosplaying/images/real"
+#define IMAGES_PER_PAGE 4
 
 @interface ImagesPreviewController : UIViewController <UISearchBarDelegate> {
 
@@ -19,6 +20,12 @@
 	UIImageView *imageView3;
 	UIImageView *imageView4;
 	
+	int offset;
+	NSString *q;
+	NSString *keyword;
+	
+	UIDatePicker *datePicker;
+	UIButton *jumpToDateButton;
 }
 
 @property (nonatomic, retain) IBOutlet UISearchBar *searchBar;
@@ -26,9 +33,15 @@
 @property (nonatomic, retain) IBOutlet UIImageView *imageView2;
 @property (nonatomic, retain) IBOutlet UIImageView *imageView3;
 @property (nonatomic, retain) IBOutlet UIImageView *imageView4;
+@property (nonatomic, retain) IBOutlet UIDatePicker *datePicker;
+@property (nonatomic, retain) IBOutlet UIButton *jumpToDateButton;
+
 
 
 - (IBAction)searchButtonPressed;
 - (IBAction)segmentPressed:(id) sender;
+- (IBAction)previousPressed;
+- (IBAction)nextPressed;
+- (IBAction)jumpToDatePressed;
 
 @end
