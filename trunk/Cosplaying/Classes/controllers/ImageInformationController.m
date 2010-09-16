d11c3d6e3069914390e7c7f387b06495b5223b90
Java2Enterprise <Java2Enterprise@28abb18a-2c56-11df-af82-b5ce7a3b21f7>
2010-09-16 14:34:39 +0000

    Working on ui

---
+++ trunk/Cosplaying/Classes/controllers/ImageInformationController.m
@@ -0,0 +1,64 @@
+//
+//  ImageInformationController.m
+//  Cosplaying
+//
+//  Created by Hello Baby on 9/16/10.
+//  Copyright 2010 Freeze!. All rights reserved.
+//
+
+#import "ImageInformationController.h"
+#import "WriteReviewController.h"
+
+@implementation ImageInformationController
+
+/*
+ // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
+        // Custom initialization
+    }
+    return self;
+}
+*/
+
+/*
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+*/
+
+/*
+// Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+*/
+
+- (void)didReceiveMemoryWarning {
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+- (void)viewDidUnload {
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+- (IBAction)writeReview:(id) sender {
+	WriteReviewController *writeReviewController = [[WriteReviewController alloc] initWithNibName:@"WriteReviewController" bundle:nil];
+	[self.navigationController pushViewController:writeReviewController animated:YES];
+	[writeReviewController release];
+}
+
+
+@end
